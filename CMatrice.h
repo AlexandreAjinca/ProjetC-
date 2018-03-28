245de48bae20ed85c2ef49c44792681a6cd1bf75
AlexandreAjinca <a.ajinca@laposte.net>
2018-03-28 09:56:11 +0200

    Create CMatrice.h

---
+++ CMatrice.h
@@ -0,0 +1,14 @@
+#pragma once
+template <class Type> class CMatrice
+{
+private:
+	unsigned int uiMATnbLignes;
+	unsigned int uiMATnbColonnes;
+	Type** ppMATmatrice;
+public:
+	CMatrice<Type>();
+	~CMatrice();
+	CMatrice<Double>(CMatrice<Type> mat, double valeur) {
+		if(Type)
+	}
+};
